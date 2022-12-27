eff80dd23705c062ffbf55248088cdbf27cb9249
Thomas Feduk <thomasfeduk@users.noreply.github.com>
2022-12-27 05:41:46 -0500

    WIP: Sample mock of static method

--- test/test_kinesis_client.py
+++ test/test_kinesis_client.py
@@ -3,7 +3,9 @@ import importlib
 import unittest
 import unittest.mock as mock
 from unittest.mock import patch
+import includes.kinesis_client as kinesis_client
 import sys
+from includes.debug import *
 
 
 class Main(unittest.TestCase):
@@ -22,5 +24,10 @@ class Main(unittest.TestCase):
         pass
 
     def test_ClientConfig(self):
+        with patch('includes.kinesis_client.ShardIteratorConfig.is_valid', create=True) as mocked_kinesis_client:
+            mocked_kinesis_client.return_value = 'boo'
+            var = kinesis_client.ShardIteratorConfig.is_valid()
+            pvdd(var)
+
         self.assertEqual(1, 2)
 
