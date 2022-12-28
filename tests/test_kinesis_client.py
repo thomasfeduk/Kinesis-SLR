fe49cd41624281de0f86bcb1c51a706011226fad
Thomas Feduk <thomasfeduk@users.noreply.github.com>
2022-12-27 19:23:08 -0500

    Switched test for required configs to test via a loop

--- tests/test_kinesis_client.py
+++ tests/test_kinesis_client.py
@@ -9,7 +9,7 @@ from includes.debug import *
 import includes.common as common
 
 
-class ClientConfigReqParams(unittest.TestCase):
+class ClientConfig(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         pass
@@ -24,100 +24,42 @@ class ClientConfigReqParams(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_stream_name(self):
-        yaml_input = {
-
-        }
+    def test_ReqConfigs_empty(self):
+        # Run the test once with an empty input config list to simulate no configs set
+        config_input = {}
         with self.assertRaises(ValueError) as ex:
-            kinesis.ConfigClient(yaml_input)
+            kinesis.ConfigClient(config_input)
         self.assertEqual(
             'config-kinesis_scraper.yaml: Missing config parameter: stream_name',
             str(ex.exception)
         )
 
-    def test_shardIds(self):
-        yaml_input = {
-            "stream_name": "x"
-        }
-        with self.assertRaises(ValueError) as ex:
-            kinesis.ConfigClient(yaml_input)
-        self.assertEqual(
-            'config-kinesis_scraper.yaml: Missing config parameter: shardIds',
-            str(ex.exception)
-        )
-
-    def test_starting_position(self):
-        yaml_input = {
-            "stream_name": "x",
-            "shardIds": [],
-        }
-        with self.assertRaises(ValueError) as ex:
-            kinesis.ConfigClient(yaml_input)
-        self.assertEqual(
-            'config-kinesis_scraper.yaml: Missing config parameter: starting_position',
-            str(ex.exception)
-        )
-
-    def test_max_total_records_per_shard(self):
-        yaml_input = {
-            "stream_name": "x",
-            "shardIds": [],
-            "starting_position": "x",
-        }
-        with self.assertRaises(ValueError) as ex:
-            kinesis.ConfigClient(yaml_input)
-        self.assertEqual(
-            'config-kinesis_scraper.yaml: Missing config parameter: max_total_records_per_shard',
-            str(ex.exception)
-        )
-
-    def test_poll_batch_size(self):
-        yaml_input = {
-            "stream_name": "x",
-            "shardIds": [],
-            "starting_position": "x",
-            "max_total_records_per_shard": "x",
-
-        }
-        with self.assertRaises(ValueError) as ex:
-            kinesis.ConfigClient(yaml_input)
-        self.assertEqual(
-            'config-kinesis_scraper.yaml: Missing config parameter: poll_batch_size',
-            str(ex.exception)
-        )
-
-    def test_poll_delay(self):
-        yaml_input = {
-            "stream_name": "x",
-            "shardIds": [],
-            "starting_position": "x",
-            "max_total_records_per_shard": "x",
-            "poll_batch_size": "x"
-
-        }
-        with self.assertRaises(ValueError) as ex:
-            kinesis.ConfigClient(yaml_input)
-        self.assertEqual(
-            'config-kinesis_scraper.yaml: Missing config parameter: poll_delay',
-            str(ex.exception)
-        )
-
-    def test_max_empty_record_polls(self):
-        yaml_input = {
-            "stream_name": "x",
-            "shardIds": [],
-            "starting_position": "x",
-            "max_total_records_per_shard": "x",
-            "poll_batch_size": "x",
-            "poll_delay": "x"
-
-        }
-        with self.assertRaises(ValueError) as ex:
-            kinesis.ConfigClient(yaml_input)
-        self.assertEqual(
-            'config-kinesis_scraper.yaml: Missing config parameter: max_empty_record_polls',
-            str(ex.exception)
-        )
+    def test_ReqConfigs(self):
+        # Must match the same list order declared in kinesis.ConfigClient._is_valid
+        required_configs = [
+            'stream_name',
+            'shardIds',
+            'starting_position',
+            'max_total_records_per_shard',
+            'poll_batch_size',
+            'poll_delay',
+            'max_empty_record_polls',
+        ]
+        config_input = {}
+        i = 0
+        for current_conf in required_configs:
+            # End the test if we already ran through all but the last item,
+            # since the last item is checked in the second to last call (index+1)
+            if i == len(required_configs)-1:
+                return
+            config_input[current_conf] = "x"
+            with self.assertRaises(ValueError) as ex:
+                kinesis.ConfigClient(config_input)
+            self.assertEqual(
+                f'config-kinesis_scraper.yaml: Missing config parameter: {required_configs[i+1]}',
+                str(ex.exception)
+            )
+            i += 1
 
     # def test_ClientConfig(self):
     #     with patch('includes.kinesis_client.ShardIteratorConfig.is_valid', create=True) as mocked_kinesis_client:
