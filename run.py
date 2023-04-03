07ccc2fd1d2ec992c59a0e45a51c6904cecb0745
Thomas Feduk <thomasfeduk@users.noreply.github.com>
2023-04-03 00:50:44 -0400

    WIP: Working on menu wording

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from includes.debug import *
 from art import tprint
 import yaml
 import os
+import textwrap
 
 
 def scrape_menu():
@@ -33,17 +34,40 @@ def display_logo():
     tprint("Kinesis-SLR", font="rounded", chr_ignore=True)
     print(lines)
 
+def print_wrapped(text: str, *, width: int = 100):
+    wrapped_text = textwrap.wrap(text, width=width)
+    for line in wrapped_text:
+        print(line)
 
 def main():
     clear_screen()
     display_logo()
-    with open("config-kinesis_scraper.yaml", "r") as f:
-        config = yaml.safe_load(f)
-    display_yaml(config)
+    # with open("config-kinesis_scraper.yaml", "r") as f:
+    #     config = yaml.safe_load(f)
+    # display_yaml(config)
 
-    print("\nChoose an option:")
-    print("1. Open Scraper Menu")
-    print("2. Open Replay Menu")
+    print("\n\n")
+    print_wrapped("Kinesis-SLR is a tool designed for reading and replaying messages from an Amazon Kinesis stream to "
+                  "local disk, then replaying those messages to one or more Lambdas. It was made to assist in "
+                  "disaster recovery situations where Lambdas may have missed consuming messages, or message "
+                  "processing failed and need to be re-attempted outside of the simple ranges provided by the AWS "
+                  "infrastructure. It allows you to connect to any point in the stream, read messages to disk "
+                  "locally, and then replay them back to a Lambda function in batches, mimicking the AWS "
+                  "Kinesis>Lambda native integration (including retry handling and bisect/bisectBatchOnFunctionError) "
+                  "and incorporates a local file-based DLQ to write failed messages.. With Kinesis-SLR, "
+                  "you can recover and reprocess missed or failed messages during previous consumption.")
+    print("Kinesis-SLR has two main components: the Scraper and the Lambda Replay.")
+    print("\n\n")
+    print_wrapped("The Scraper reads messages from an Amazon Kinesis stream between any two points within the stream and writes them locally to disk.")
+    print("\n\n")
+    print_wrapped("The lambda replay reads messages from disk and replays them back to a Lambda function in batches, ")
+    print("\n\n")
+    print_wrapped("This tool is designed to fill the gap in the current AWS tooling such as KCL (Kinesis Client Library), by providing a small flexible way to scrape and replay arbitrary ranges of messages in a stream.")
+    print("\n\n")
+
+    print("\nWhat would you like to do?")
+    print("1. Open Kinesis Scraper Menu")
+    print("2. Open Lambda Re-play Menu")
     print("3. Quit")
     choice = input("> ")
     os.system("cls" if os.name == "nt" else "clear")
