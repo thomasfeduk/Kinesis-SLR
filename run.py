7d8db3af2a643203c8844023d27b7002825ed3d8
Thomas Feduk <thomasfeduk@users.noreply.github.com>
2023-04-02 22:28:55 -0400

    WIP: Adding basic menu switch

--- run.py
+++ run.py
@@ -1,70 +1,62 @@
-import scrape
 from includes.debug import *
 from art import tprint
 import yaml
 import os
 
-def clear_screen():
-        os.system('cls')
-        os.system('clear')
 
-class YamlTable:
-    def __init__(self, data):
-        self.data = data
-
-    def display_table(self):
-        for key, value in config.items():
-            print("{:<30}{}".format(key, value))
-
-    def prompt_user(self):
-        print("\nChoose an option:")
-        print("1. Option 1")
-        print("2. Option 2")
-        print("3. Option 3")
-        choice = input("> ")
-        os.system("cls" if os.name == "nt" else "clear")
-        if choice == "1":
-            self.function1()
-        elif choice == "2":
-            self.function2()
-        elif choice == "3":
-            self.function3()
-        else:
-            print("Invalid choice")
-
-    def function1(self):
-        print("Running function 1")
-
-    def function2(self):
-        print("Running function 2")
-
-    def function3(self):
-        print("Running function 3")
-
-
-if __name__ == "__main__":
-    # scrape.main_kinesis()
-    # scrape.main_lambda()
-
-    with open("config-kinesis_scraper.yaml", "r") as f:
-        config = yaml.safe_load(f)
-
-
-    table = YamlTable(config)
-    table.display_table()
-    table.prompt_user()
-
-
-    die('end')
-
-
+def scrape_menu():
     clear_screen()
+    display_logo()
+    die('scrape menu')
+
+def replay_menu():
+    clear_screen()
+    display_logo()
+    die('replay menu')
+
+
+def clear_screen():
+    os.system('cls')
+    os.system('clear')
+
+
+def display_yaml(config):
+    for key, value in config.items():
+        print("{:<30}{}".format(key, value))
+
+
+def display_logo():
     lines = ''.join(['_' for _ in range(103)])
     print(f"{lines}\n\n")
     # tprint("Kinesis-SLR", font="slant", chr_ignore=True)
     # tprint("Kinesis-SLR", font="larry", chr_ignore=True)
     tprint("Kinesis-SLR", font="rounded", chr_ignore=True)
     print(lines)
-    die()
 
-    # Create a Bo
+
+def main():
+    clear_screen()
+    display_logo()
+    with open("config-kinesis_scraper.yaml", "r") as f:
+        config = yaml.safe_load(f)
+    display_yaml(config)
+
+    print("\nChoose an option:")
+    print("1. Open Scraper Menu")
+    print("2. Open Replay Menu")
+    print("3. Quit")
+    choice = input("> ")
+    os.system("cls" if os.name == "nt" else "clear")
+    if choice == "1":
+        scrape_menu()
+    elif choice == "2":
+        replay_menu()
+    else:
+        print("Exiting...")
+
+
+if __name__ == "__main__":
+    # scrape.main_kinesis()
+    # scrape.main_lambda()
+
+    main()
